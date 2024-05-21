594f49b0bec8a7f05ea63e9a369cfe27509d6a5f
fastestsunil <sunil141292@gmail.com>
2024-05-21 22:48:09 +0530

    changes

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -5,14 +5,11 @@ import Link from 'next/link';
 
 export default function Logo({ className }: { className?: string }) {
   return (
-    <Link
-      href="/"
-      className={cn('relative overflow-hidden h-20 w-[50%]', className)}
-    >
+    <Link href="/" className={cn('relative overflow-hidden', className)}>
       <Image
         alt="coast to coast logo"
         src="/images/brand/logo.png"
-        className={cn('object-cover w-36', className)}
+        className={cn('object-cover', className)}
         priority
         fill
       />
